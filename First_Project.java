9a630f27d43338edf288124c0d995916c8bd2c7d
Uma1deep <162682847+Uma1deep@users.noreply.github.com>
2024-03-08 14:13:54 +0530

    Week1/Day1

---
+++ First_Project.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class First_Project {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Test leaf");
+	}
+}
---
+++ learnForloop.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class learnForloop {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		
+		for(int i=1; i<=num; i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println("Odd numbers are:" +i);
+			}
+		}
+
+	}
+
+}
